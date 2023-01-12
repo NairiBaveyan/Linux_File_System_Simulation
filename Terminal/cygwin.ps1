2204bfa98b319e743e426b17e835aba2ee069cd9
NairiBaveyan <nairikham@gmail.com>
2023-01-12 19:02:34 +0400

    Testing awk and gawk commands

--- Terminal/cygwin.ps1
+++ Terminal/cygwin.ps1
@@ -4,6 +4,17 @@ $verific_path = Get-Location
 $OldPATH = $PATH
 $env:PATH = (Test-Path -Path "C:\cygwin64\bin") ? "C:\cygwin64\bin\" : "C:\cygwin\bin\"
 $Cygwin = $env:PATH + "bash.exe"
+$awk = "awk"
+$gawk = "gawk"
+
+Write-Output "awk and gawk"
+& $awk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' abclib.dsp > tmp.dsp
+& $gawk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' abclib.dsp > tmp.dsp
+
+Write-Output "C:\cygwin\bin\gawk"
+$Cyg_gawk = $env:PATH + "gawk"
+& $Cyg_gawk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' abclib.dsp > tmp.dsp
+
 
 #$env:PATH -split ";"
 sed -help
@@ -15,6 +26,8 @@ $argB="bison -help"
 
 & $Cygwin $arg0 $argF
 & $Cygwin $arg0 $argB
+
+& $Cygwin $arg0 "gawk" 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' abclib.dsp > tmp.dsp
 
 $env:PATH = $OldPATH
 Write-Output "Cygwin finished"

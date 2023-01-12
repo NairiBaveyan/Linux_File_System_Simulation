b5eef0083b1618c9a4194fa7cf0435a1cfd80bcf
NairiBaveyan <nairikham@gmail.com>
2023-01-12 19:23:15 +0400

    Testing awk and gawk commands

--- Terminal/cygwin.ps1
+++ Terminal/cygwin.ps1
@@ -31,3 +31,17 @@ $argB="bison -help"
 
 $env:PATH = $OldPATH
 Write-Output "Cygwin finished"
+
+
+
+
+Write-Output "abc action script start "
+
+
+sed -i 's#ABC_USE_PTHREADS\"#ABC_DONT_USE_PTHREADS\" /D \"_ALLOW_KEYWORD_MACROS=1\"#g' .\*.dsp
+awk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' .\abclib.dsp > .\tmp.dsp
+copy .\tmp.dsp .\abclib.dsp
+del .\tmp.dsp
+unix2dos .\*.dsp
+<# executing MsDevShell #>
+pwsh.exe .\function.ps1

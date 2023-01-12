abea0e97ddd376a8c3b8ab9668036ab2a2ffded0
NairiBaveyan <nairikham@gmail.com>
2023-01-12 18:29:35 +0400

    Checking the existence of awk and gawk

--- action.ps1
+++ action.ps1
@@ -1,5 +1,10 @@
 Write-Output "Build Visual Studio project"
 
+Write-Output "awk"
+awk
+Write-Output "gawk"
+gawk
+
 sed --help
 msbuild filesystem.sln /p:Configuration=Release /p:PlatformTarget=x64
 

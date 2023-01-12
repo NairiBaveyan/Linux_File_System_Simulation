419f5843e6fbfbf74c8e196b461354a63b36eaad
NairiBaveyan <nairikham@gmail.com>
2023-01-12 18:38:16 +0400

    Checking the existence of awk and gawk

--- action.ps1
+++ action.ps1
@@ -1,9 +1,9 @@
 Write-Output "Build Visual Studio project"
 
 Write-Output "awk"
-awk
+awk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' abclib.dsp > tmp.dsp
 Write-Output "gawk"
-gawk
+gawk 'BEGIN { del=0; } /# Begin Group "uap"/ { del=1; } /# End Group/ { if( del > 0 ) {del=0; next;} } del==0 {print;} ' abclib.dsp > tmp.dsp
 
 sed --help
 msbuild filesystem.sln /p:Configuration=Release /p:PlatformTarget=x64

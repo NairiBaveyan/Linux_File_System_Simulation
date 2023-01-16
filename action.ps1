1daff744fb8fb3fe44fcc7fb5d9ad8352373b995
Nairi Baveyan <nairikham@gmail.com>
2023-01-16 18:24:52 +0400

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -1,21 +1,24 @@
 Write-Output "Build Visual Studio project"
 
 $OldPath = $env:PATH
-$env:PATH = "c:\\cygwin64\\bin"
+$env:PATH = "c:\\cygwin64\\bin\\"
 $BASH = $env:PATH + "\\bash.exe"
 
 Write-Output "LN RUNS"
 
-bash -c "ln -sf vhdl_1993/bin vdbs_1993"
+c:\\cygwin64\\bin\\bash -c "ln -sf vhdl_1993/bin vdbs_1993"
 bash.exe -c "ln -sf vhdl_2008/bin vdbs_2008"
 bash -c "ln -sf vhdl_2019/bin vdbs_2019"
+c:\\cygwin64\\bin\\wget -nc -O pmgen.py https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py
+& "c:\\cygwin64\\bin\\wget" "-nc" "-O" "pmgen.py" "https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py"
+
 if($?)
 {
   Write-Output "true"
 }
 Write-Output "MAKE RUNS"
 
-bash -c "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
+c:\\cygwin64\\bin\\bash -c "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
 
 
 Write-Output "Action ends"

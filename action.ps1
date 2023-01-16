67b4e74f73cc66296395e636931b38ce17e6803c
NairiBaveyan <nairikham@gmail.com>
2023-01-16 18:30:04 +0400

    Added new scripts

--- action.ps1
+++ action.ps1
@@ -1,16 +1,17 @@
 Write-Output "Build Visual Studio project"
 
-$OldPath = $env:PATH
-$env:PATH = "c:\\cygwin64\\bin\\"
-$BASH = $env:PATH + "\\bash.exe"
+$OldPATH = $PATH
+$env:PATH = (Test-Path -Path "C:\cygwin64\bin") ? "C:\cygwin64\bin\" : "C:\cygwin\bin\"
+$env:PATH -split ";"
+$Cygwin = $env:PATH + "bash.exe"
+$arg = "-c"
 
 Write-Output "LN RUNS"
 
-c:\\cygwin64\\bin\\bash -c "ln -sf vhdl_1993/bin vdbs_1993"
-bash.exe -c "ln -sf vhdl_2008/bin vdbs_2008"
-bash -c "ln -sf vhdl_2019/bin vdbs_2019"
-c:\\cygwin64\\bin\\wget -nc -O pmgen.py https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py
-& "c:\\cygwin64\\bin\\wget" "-nc" "-O" "pmgen.py" "https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py"
+& $Cygwin $arg "ln -sf vhdl_1993/bin vdbs_1993"
+& $Cygwin $arg "ln -sf vhdl_2008/bin vdbs_2008"
+& $Cygwin $arg "ln -sf vhdl_2019/bin vdbs_2019"
+& $Cygwin $arg "wget -nc -O pmgen.py https://raw.githubusercontent.com/YosysHQ/yosys/master/passes/pmgen/pmgen.py"
 
 if($?)
 {
@@ -18,7 +19,7 @@ if($?)
 }
 Write-Output "MAKE RUNS"
 
-c:\\cygwin64\\bin\\bash -c "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
+& $Cygwin $arg "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
 
 
 Write-Output "Action ends"

825abefee05551635d27b0179d54cdbe265b8d7f
Nairi Baveyan <nairikham@gmail.com>
2023-01-16 18:05:23 +0400

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -5,14 +5,18 @@ $env:PATH = "c:\\cygwin64\\bin"
 $BASH = $env:PATH + "\\bash.exe"
 
 Write-Output "LN RUNS"
+
 $BASH -c "ln -sf vhdl_1987/bin vdbs_1987"
 c:\\cygwin64\\bin\\bash -c "ln -sf vhdl_1993/bin vdbs_1993"
-& $BASH "-c" "ln -sf vhdl_2008/bin vdbs_2008"
-& $BASH "-c" "ln -sf vhdl_2019/bin vdbs_2019"
-
+$BASH -c "ln -sf vhdl_2008/bin vdbs_2008"
+$BASH -c "ln -sf vhdl_2019/bin vdbs_2019"
+if($?)
+{
+  Write-Output "true"
+}
 Write-Output "MAKE RUNS"
 
-c:\\cygwin64\\bin\\bash.exe -c "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
+$BASH -c "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
 
 
 Write-Output "Action ends"

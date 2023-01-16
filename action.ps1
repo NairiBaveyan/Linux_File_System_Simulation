8d006927782828f37bd6cd8551339e42d5e9f0af
Nairi Baveyan <nairikham@gmail.com>
2023-01-16 18:15:55 +0400

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -8,14 +8,14 @@ Write-Output "LN RUNS"
 
 bash -c "ln -sf vhdl_1993/bin vdbs_1993"
 bash.exe -c "ln -sf vhdl_2008/bin vdbs_2008"
-$BASH -c "ln -sf vhdl_2019/bin vdbs_2019"
+bash -c "ln -sf vhdl_2019/bin vdbs_2019"
 if($?)
 {
   Write-Output "true"
 }
 Write-Output "MAKE RUNS"
 
-$BASH -c "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
+bash -c "make genfiles " "SHELL=C:\\cygwin64\\bin\\bash"
 
 
 Write-Output "Action ends"
